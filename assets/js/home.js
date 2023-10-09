8ed3959c7a5b8a9f2a2a9eec7c075d922e155d70
maheeshak <maheeshafernando18@gmail.com>
2023-10-09 16:11:19 +0530

    change SPA application

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,24 +1,71 @@
+$("#customer").css('display','none');
+$("#item").css('display','none');
+$("#order").css('display','none');
+
+
+/*Buttons*/
 $('#customer_btn').on("click", () => {
-    const customerSection = $('#customer');
-    const customerSectionTop = customerSection.offset().top;
-    $('html, body').animate({
-        scrollTop: customerSectionTop
-    }, 1000);
+    event.preventDefault();
+    $("#customer").css('display','block');
+    $("#item").css('display','none');
+    $("#order").css('display','none');
+    $("#home_main").css('display','none');
 
 })
 $('#item_btn').on("click", () => {
-    const customerSection = $('#item');
-    const customerSectionTop = customerSection.offset().top;
-    $('html, body').animate({
-        scrollTop: customerSectionTop
-    }, 1000);
+    event.preventDefault();
+    $("#customer").css('display','none');
+    $("#item").css('display','block');
+    $("#order").css('display','none');
+    $("#home_main").css('display','none');
+
 
 })
 $('#order_btn').on("click", () => {
-    const customerSection = $('#order');
-    const customerSectionTop = customerSection.offset().top;
-    $('html, body').animate({
-        scrollTop: customerSectionTop
-    }, 1000);
+    event.preventDefault();
+    $("#customer").css('display','none');
+    $("#item").css('display','none');
+    $("#order").css('display','block');
+    $("#home_main").css('display','none');
 
 })
+
+
+/*Nav Bar*/
+$('#nav_list>a').eq(0).on("click", () => {
+    event.preventDefault();
+    $("#customer").css('display','none');
+    $("#item").css('display','none');
+    $("#order").css('display','none');
+    $("#home_main").css('display','block');
+
+})
+
+$('#nav_list>a').eq(1).on("click", () => {
+    event.preventDefault();
+    $("#customer").css('display','block');
+    $("#item").css('display','none');
+    $("#order").css('display','none');
+    $("#home_main").css('display','none');
+
+})
+
+$('#nav_list>a').eq(2).on("click", () => {
+    event.preventDefault();
+    $("#customer").css('display','none');
+    $("#item").css('display','block');
+    $("#order").css('display','none');
+    $("#home_main").css('display','none');
+
+})
+
+
+$('#nav_list>a').eq(3).on("click", () => {
+    event.preventDefault();
+    $("#customer").css('display','none');
+    $("#item").css('display','none');
+    $("#order").css('display','block');
+    $("#home_main").css('display','none');
+
+})
+
